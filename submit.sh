e842f0ee2dfaa77c56e8b569f7ec2ee2f1344eef
Masahiro Nakao <masahiro.nakao@riken.jp>
2020-05-17 10:06:31 +0900

    minor fix

--- submit.sh
+++ submit.sh
@@ -5,7 +5,6 @@
 #PJM -L "elapse=0:10:00"
 #PJM --name "n"
 
-PROBLEM_SIZE=30
 export TWOD_R=8
-mpiexec -mca btl_tofu_eager_limit 512000 -mca mpi_print_stats 3 runnable ${PROBLEM_SIZE}
+mpiexec ./runnable 30
 echo $SECONDS
